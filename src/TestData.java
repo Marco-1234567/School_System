e8fb6be4f817ac1c2e02756fe5cacf37b16dbedc
Marco <laasonen.marco@yahoo.com>
2025-10-07 15:03:33 +0200

    TestData class in progress

---
+++ src/TestData.java
@@ -0,0 +1,12 @@
+import Persons.Person;
+import Persons.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestData {
+
+    private List<Person> students = new ArrayList<>( List.of(
+            new Student("")
+    ));
+}
