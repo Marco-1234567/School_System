066c904ae04b248aa3820559db276e5dd7addd63
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-10-08 13:53:51 +0200

    Uppdate GradeScheme and make LetterGradeScheme and PassFailScheme

--- src/grade/PassFailScheme.java
+++ src/grade/PassFailScheme.java
@@ -1,4 +1,12 @@
 package grade;
 
-public class PassFailScheme {
+public class PassFailScheme implements GradeScheme{
+    @Override
+    public Grade evaluateGrade(int scoreValue) {
+        if(scoreValue >= 50){
+            return Grade.Passed;
+        }else{
+            return Grade.Failed;
+        }
+    }
 }
