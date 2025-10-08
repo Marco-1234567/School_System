22f9147c6a58d915e073f964546f36734e9e88da
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-10-08 15:29:52 +0200

    Create constructor in GradeScheme

--- src/grade/GradeScheme.java
+++ src/grade/GradeScheme.java
@@ -2,4 +2,9 @@ package grade;
 
 public interface GradeScheme {
     Grade evaluateGrade(int scoreValue);
+
+    public default GradeScheme getGradeScheme(){
+        return GradeScheme.this;
+    };
+
 }
